11196b61df4b243a71b194ffe3e174cdbd99e3c3
Mehmet Telci <umittelci@gmail.com>
2013-02-26 10:40:05 -0600

    Merge WinForms first version

---
+++ MergeIndesign/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace MergeIndesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIndesign = new System.Windows.Forms.Button();
+            this.txtLabel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbColor = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbFont = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbFSize = new System.Windows.Forms.ComboBox();
+            this.chkFit = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnIndesign
+            // 
+            this.btnIndesign.Location = new System.Drawing.Point(274, 234);
+            this.btnIndesign.Name = "btnIndesign";
+            this.btnIndesign.Size = new System.Drawing.Size(226, 40);
+            this.btnIndesign.TabIndex = 0;
+            this.btnIndesign.Text = "InDesign Process";
+            this.btnIndesign.UseVisualStyleBackColor = true;
+            this.btnIndesign.Click += new System.EventHandler(this.btnIndesign_Click);
+            // 
+            // txtLabel
+            // 
+            this.txtLabel.Location = new System.Drawing.Point(16, 33);
+            this.txtLabel.Multiline = true;
+            this.txtLabel.Name = "txtLabel";
+            this.txtLabel.Size = new System.Drawing.Size(484, 58);
+            this.txtLabel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customized Label";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Color";
+            // 
+            // cmbColor
+            // 
+            this.cmbColor.FormattingEnabled = true;
+            this.cmbColor.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue"});
+            this.cmbColor.Location = new System.Drawing.Point(20, 186);
+            this.cmbColor.Name = "cmbColor";
+            this.cmbColor.Size = new System.Drawing.Size(227, 24);
+            this.cmbColor.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Font Family";
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.FormattingEnabled = true;
+            this.cmbFont.Items.AddRange(new object[] {
+            "Broadway",
+            "Myriad Pro",
+            "Chiller",
+            "Verdana"});
+            this.cmbFont.Location = new System.Drawing.Point(21, 133);
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(226, 24);
+            this.cmbFont.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Results";
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.ItemHeight = 16;
+            this.lstResult.Location = new System.Drawing.Point(13, 307);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(505, 84);
+            this.lstResult.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(271, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Font Size";
+            // 
+            // cmbFSize
+            // 
+            this.cmbFSize.FormattingEnabled = true;
+            this.cmbFSize.Items.AddRange(new object[] {
+            "50",
+            "75",
+            "90",
+            "100",
+            "120"});
+            this.cmbFSize.Location = new System.Drawing.Point(274, 133);
+            this.cmbFSize.Name = "cmbFSize";
+            this.cmbFSize.Size = new System.Drawing.Size(213, 24);
+            this.cmbFSize.TabIndex = 10;
+            // 
+            // chkFit
+            // 
+            this.chkFit.AutoSize = true;
+            this.chkFit.Location = new System.Drawing.Point(274, 186);
+            this.chkFit.Name = "chkFit";
+            this.chkFit.Size = new System.Drawing.Size(101, 21);
+            this.chkFit.TabIndex = 11;
+            this.chkFit.Text = "Fit to frame";
+            this.chkFit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 395);
+            this.Controls.Add(this.chkFit);
+            this.Controls.Add(this.cmbFSize);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbFont);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbColor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLabel);
+            this.Controls.Add(this.btnIndesign);
+            this.Name = "Form1";
+            this.Text = "Customized Art Work";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIndesign;
+        private System.Windows.Forms.TextBox txtLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbColor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbFont;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbFSize;
+        private System.Windows.Forms.CheckBox chkFit;
+    }
+}
+
---
+++ MergeIndesign/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InDesign;
+
+namespace MergeIndesign
+{
+    public partial class Form1 : Form
+    {
+        InDesign.Application idApp;
+        InDesign.Document idDoc;
+        //InDesign.Layer idLayer;
+        InDesign.Page idPage;
+        InDesign.Text idText;
+        InDesign.Color idColor;
+
+        InDesign.Color idCRed;
+        InDesign.Color idCBlue;
+        InDesign.Color idCGreen;
+        //InDesign.TextFrame idTextframe;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initializeForm();
+        }
+
+        
+
+        private void initializeForm()
+        {
+            idApp = (InDesign.Application) COMCreateObject("InDesign.Application");
+
+
+            System.Threading.Thread.Sleep(2000);
+            idDoc = idApp.Open("C:\\prints\\indesign\\DL013GN2T Folder\\DL013GN2T2.indd", true, idOpenOptions.idDefault);
+
+            idPage = idDoc.Pages.FirstItem();
+
+
+            idCRed = setColor("Red", InDesign.idColorSpace.idRGB
+                , InDesign.idColorModel.idProcess, new int[] { 255, 0, 0 });
+            idCGreen = setColor("Green", InDesign.idColorSpace.idRGB
+                , InDesign.idColorModel.idProcess, new int[] { 0, 255, 0 });
+            idCBlue = setColor("Blue", InDesign.idColorSpace.idRGB
+                , InDesign.idColorModel.idProcess, new int[] { 0, 0, 255 });
+            
+             //= .FirstItem();
+
+            //idPage.TextFrames.Count
+            //idDoc = idApp.Documents.Add();
+
+            //textframe = idDoc.TextFrames.Add();
+            //textframe.GeometricBounds = new string[] {"6p", "6p", "24p", "24p"};
+            //textframe.Contents = "TESSSSSSSSSSSSSTTTTTTTTTTTT";
+            //idLayer = idDoc.Layers.Add();
+
+
+                //idApp.Open("C:\\prints\\indesign\\DL013GN2T Folder\\DL013GN2T.indd", InDesign.idColorSpace.idRGB);
+        }
+
+        public static object COMCreateObject(string sProgID)
+        {
+            // We get the type using just the ProgID
+            Type oType = Type.GetTypeFromProgID(sProgID);
+            if (oType != null)
+            {
+                return Activator.CreateInstance(oType) ;
+            }
+
+            return null;
+        }
+
+        private void btnIndesign_Click(object sender, EventArgs e)
+        {
+            string customizedText = txtLabel.Text.Trim();
+            string fontName = cmbFont.SelectedItem==null ? "" : cmbFont.SelectedItem.ToString();
+            string fontSize = cmbFSize.SelectedItem == null ? "" : cmbFSize.SelectedItem.ToString();
+            lstResult.Items.Clear();
+
+            idColor = null;
+            if (cmbColor.SelectedItem != null)
+            {
+                if (cmbColor.SelectedItem.Equals("Red"))
+                    idColor = idCRed;
+                else if (cmbColor.SelectedItem.Equals("Green"))
+                    idColor = idCGreen;
+                else if (cmbColor.SelectedItem.Equals("Blue"))
+                    idColor = idCBlue;
+            }
+
+            InDesign.TextFrame idTextframeStr;
+            InDesign.TextFrame idTextframeCut;
+
+            idTextframeCut = idPage.TextFrames["<cutlabel>"];
+            idTextframeCut.Contents = customizedText;
+            idText = idTextframeCut.Texts.FirstItem();
+                if( !fontName.Equals("") )
+                    idText.AppliedFont = fontName;
+                if (!fontSize.Equals(""))
+                    idText.PointSize = fontSize;
+
+            if (idTextframeCut.Overflows)
+            {
+                lstResult.Items.Add("Label Cut overflows");
+                if (chkFit.Checked)
+                {
+                    idTextframeCut.Fit(idFitOptions.idFrameToContent);
+                    lstResult.Items.Add("Label Cut Fit adjusted");
+                }
+            }
+
+            idTextframeStr = idPage.TextFrames["<strlabel>"];
+            idTextframeStr.Contents = customizedText;
+            //idTextframeStr.FillColor = "Blue";
+            idText = idTextframeStr.Texts.FirstItem();
+            //idText.FontStyle = "Bold";
+            if (!fontName.Equals(""))
+                idText.AppliedFont = fontName;
+            if (!fontSize.Equals(""))
+                idText.PointSize = fontSize;
+
+            //idText.AppliedFont = fontName;
+            //idText.PointSize = fontSize;
+            if (idColor != null)
+                idText.FillColor = idColor;
+
+            if (idTextframeStr.Overflows)
+            {
+                lstResult.Items.Add("Label String overflows");
+                if (chkFit.Checked)
+                {
+                    idTextframeStr.Fit(idFitOptions.idFrameToContent);
+                    lstResult.Items.Add("Label String Fit adjusted");
+                }
+            }
+
+
+            //idText.Preferences.
+
+            //foreach (InDesign.TextFrame idTextframe in idPage.TextFrames)
+            //{
+            //    string nameoftxtframe = idTextframe.Name;
+            //    string nameoftxtframe2 = idTextframe.Label;
+            //    string text = idTextframe.Contents;
+            //    idTextframe.Contents = customizedText;
+
+            //}
+
+        }
+
+        private InDesign.Color setColor( string colorName, InDesign.idColorSpace colorSpace
+            , InDesign.idColorModel colorModel, int[]  colorRGB )
+        {
+            InDesign.Color customColor;
+            customColor = idDoc.Colors.Add();
+            customColor.Name = colorName;
+
+            customColor.Space = colorSpace;
+            customColor.Model = colorModel;
+            customColor.ColorValue = colorRGB;
+
+            return customColor;
+
+        }
+    }
+}
